3091d0803521c884719a2a7a5736a13eed9ec9a1
KKNIRY <novcry@gmail.com>
2021-07-13 09:43:22 +0800

    Time: 44 ms (14.61%), Space: 14.1 MB (75.79%) - LeetHub

---
+++ remove-element/remove-element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        valid_pos = 0
+        for num in nums:
+            if num != val:
+                nums[valid_pos] = num
+                valid_pos+=1
+        nums[valid_pos:]='_'
+        return valid_pos
