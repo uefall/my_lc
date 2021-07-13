6b8bb354ce8fd2fe8bf418f2b9d9581f826194ae
KKNIRY <novcry@gmail.com>
2021-07-13 13:43:27 +0800

    Time: 56 ms (84.83%), Space: 15.4 MB (19.14%) - LeetHub

---
+++ two-sum/two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        check_d = {}
+        for idx,num in enumerate(nums):
+            if target-num in check_d:
+                return [idx,check_d[target-num]]
+            else:
+                check_d[num] = idx
+        
+                
