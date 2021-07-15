098918326f4beb301b79d5cb83439512f8443354
KKNIRY <novcry@gmail.com>
2021-07-15 08:49:34 +0800

    Time: 36 ms (98.33%), Space: 14.2 MB (89.43%) - LeetHub

---
+++ find-peak-element/find-peak-element.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        l_flg=True
+        for idx,v in enumerate(nums):
+            if idx==0:
+                last_v = v
+            else:
+                if v < last_v:
+                    if l_flg:
+                        return idx-1
+                    l_flg = False
+                else:
+                    l_flg = True
+                    
+                last_v = v
+        return idx
